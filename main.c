10027b4c8c5c80471c428c4d8801f07637fd7787
sonny070701 <ie726671teso.mx>
2022-09-26 15:57:52 -0500

    end

--- main.c
+++ main.c
@@ -5,58 +5,141 @@
  * Sonny Ceja Celis
  */
 
-//Headers internos//
-#include <stdio.h>
-#include "gpio.h"
-#include "wdog.h"
-#include "rgb.h"
+#include "fsl_debug_console.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
 
-//Headers de drivers
 #include "fsl_wdog.h"
 #include "fsl_rcm.h"
+#include "config.h"
+#include "gpio.h"
 
-//Variables del wdog
-WDOG_Type *wdog_base = WDOG;
-RCM_Type *rcm_base   = RCM;
+// count
+#define WDOG_WCT_INSTRUCITON_COUNT (256U)
 
-// Valor del timer //
+//pointers
+static WDOG_Type *wdog_base = WDOG;
+static RCM_Type *rcm_base   = RCM;
+
+//Funciones del wdog//
+static void WaitWctClose(WDOG_Type *base)
+{
+    /* Accessing register by bus clock */
+    for (uint32_t i = 0; i < WDOG_WCT_INSTRUCITON_COUNT; i++)
+    {
+        (void)base->RSTCNT;
+    }
+}
+
 static inline uint32_t GetTimerOutputValue(WDOG_Type *base)
 {
     return (uint32_t)((((uint32_t)base->TMROUTH) << 16U) | (base->TMROUTL));
 }
 
-int main(void) {
-
-	GPIO_init();
-
-	uint8_t input_port_a = false;
+int main(void)
+{
     uint16_t wdog_reset_count = 0;
-    uint8_t contador = 0;
     wdog_config_t config;
 
+    GPIO_init();
+
+    /*If not wdog reset*/
     if (!(RCM_GetPreviousResetSources(rcm_base) & kRCM_SourceWdog))
-        {
-            WDOG_ClearResetCount(wdog_base);
-        }
+    {
+        WDOG_ClearResetCount(wdog_base);
+    }
+
     wdog_reset_count = WDOG_GetResetCount(wdog_base);
 
-	for(;;)
-	{
-		input_port_a = !GPIO_PinRead(GPIOA, 4);
+    if (wdog_reset_count == 0)
+    {
+        WDOG_GetDefaultConfig(&config);
+        config.timeoutValue = 0x1002U;
+        WDOG_Init(wdog_base, &config);
+        WaitWctClose(wdog_base);
 
-		if(false != input_port_a)
-		{
-			contador = +1;
-		}
-
-		if(input_port_a)
-		{
-			while(1);
-		}
-
-		WDOG_Refresh(WDOG);
-		while(GetTimerOutputValue(wdog_base) < (config.timeoutValue >> 3U))
-		{
+        for (;;)
+        {
+            WDOG_Refresh(wdog_base);
+            while (GetTimerOutputValue(wdog_base) < (config.timeoutValue >> 3U)) // esto va siempre del feed / refresh
+            {
+            }
+            if(!(GPIO_PinRead(SW2_GPIO, SW2_PIN))){
+            	for(;;){
+            	};
+            }
         }
-	}
+        /* wait for wdog timeout reset */
+    }
+    else if (wdog_reset_count == 1)
+    {
+
+        GPIO_PortClear(BLUE_LED_GPIO, 1 << 21);
+        WDOG_GetDefaultConfig(&config);
+        config.timeoutValue = 0x1002U; // es el tiempo limite que tiene para responder
+        WDOG_Init(wdog_base, &config);
+        WaitWctClose(wdog_base);
+        for(;;)
+        {
+            WDOG_Refresh(wdog_base);
+            while (GetTimerOutputValue(wdog_base) < (config.timeoutValue >> 3U)) // esto va siempre del feed / refresh
+            {
+            }
+            if(!(GPIO_PinRead(SW2_GPIO, SW2_PIN)))
+            {
+            	for(;;)
+            	{
+            	}
+            }
+        }
+
+    }
+    else if (wdog_reset_count == 2)
+    {
+    	GPIO_PortClear(RED_LED_GPIO, 1 << 22);
+    	WDOG_GetDefaultConfig(&config);
+    	config.timeoutValue = 0x1002U;
+    	WDOG_Init(wdog_base, &config);
+    	WaitWctClose(wdog_base);
+    	for(;;)
+    	{
+    		WDOG_Refresh(wdog_base);
+
+    		while (GetTimerOutputValue(wdog_base) < (config.timeoutValue >> 3U)) // esto va siempre del feed / refresh
+    		{
+    		}
+    		if(!(GPIO_PinRead(SW2_GPIO, SW2_PIN)))
+    		{
+    			for(;;)
+    			{
+    			}
+    		}
+    	}
+    }
+    else if(wdog_reset_count == 3)
+    {
+    	GPIO_PortClear(RED_LED_GPIO,  1 << 22);
+    	GPIO_PortClear(BLUE_LED_GPIO, 1 << 21);
+    	WDOG_GetDefaultConfig(&config);
+    	config.timeoutValue = 0x1002U; // es el tiempo limite que tiene para responder
+    	WDOG_Init(wdog_base, &config);
+    	WaitWctClose(wdog_base);
+    	for(;;)
+    	{
+    		WDOG_Refresh(wdog_base);
+    		while (GetTimerOutputValue(wdog_base) < (config.timeoutValue >> 3U)) // esto va siempre del feed / refresh
+    		{
+    		}
+        	if(!(GPIO_PinRead(SW2_GPIO, SW2_PIN)))
+        	{
+        		for(;;)
+        		{
+        	    }
+        	}
+    	}
+    }
+    while (1)
+    {
+    }
 }
